f88a4e7233bfcb3c2f22eadac5524de404a037c9
Dilip Kumar Patnaik Sadasivani <36917045+dsadasivani@users.noreply.github.com>
2022-10-07 10:13:17 +0530

    updating with new changes (#3)
    * heroku build script
    
    * script corrected
    
    * Update package.json
    
    * enhancements done
    
    * navigation mobile view fixed
    
    * styling issues fixed
    
    * tab icon changed to logo
    
    * improved UI and added wip page
    
    * order service and table added
    
    * prod url added
    
    * table config
    
    * ui enhancements
    
    * loading indicator added
    
    * default sort added
    
    * content changes
    
    * Update README.md
    
    * Dashboard error scenario handled
    
    * infinity scroll implmented
    
    * infinite scrolling enhanced
    
    * loading spinner alignment changed
    
    * loading logic improved
    
    * css magic
    
    * added material snackbar for alerts
    
    * responsive header added
    
    * services module moved to app root
    
    * Create order screen - First draft
    
    * create order form draft 2
    
    * create order page - UI changes final draft
    
    * create order service
    
    order creation backend code integration completed
    
    * minor bugs
    
    * commented test code
    
    * save order dialog added
    
    * bundle size budget increased by 1mb
    
    * github actions CICD added to deploy in AWS s3
    
    * back end api endpoint modified to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://iccservicesdev.us-east-1.elasticbeanstalk.com/',
+  baseUrl: 'https://api.indoconstructionchemicals.co.in/',
 };
