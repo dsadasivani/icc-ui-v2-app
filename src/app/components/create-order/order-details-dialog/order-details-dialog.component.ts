c168bad5af2199ba9cb7a79e26599b497f3523fe
Dilip Kumar Patnaik Sadasivani <dsadasivani@gmail.com>
2023-04-24 23:54:53 +0530

    Added Validations & cosmetic changes

--- src/app/components/create-order/order-details-dialog/order-details-dialog.component.ts
+++ src/app/components/create-order/order-details-dialog/order-details-dialog.component.ts
@@ -8,6 +8,56 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class OrderDetailsDialogComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  getProductsAbsoluteValue(): number {
+    let value = 0;
+    if (this.data.product1.productSelected) {
+      value += this.data.product1.quantity * this.data.product1.unitPrice;
+    }
+    if (this.data.product2.productSelected) {
+      value += this.data.product2.quantity * this.data.product2.unitPrice;
+    }
+    if (this.data.product3.productSelected) {
+      value += this.data.product3.quantity * this.data.product3.unitPrice;
+    }
+    return value;
+  }
+  getDiscount(discountType: string): number {
+    let value = 0;
+    value = this.getProductsAbsoluteValue();
+    if (discountType === 'TRADE') {
+      value = value * (this.data.tradeDiscountValue / 100);
+    }
+    if (discountType === 'CASH') {
+      if (this.data.tradeDiscount) {
+        value -= this.getDiscount('TRADE');
+      }
+      value = value * (this.data.cashDiscountValue / 100);
+    }
+    return value;
+  }
+  getTaxValue(): number {
+    let value = 0;
+    value = this.getProductsAbsoluteValue();
+    if (this.data.tradeDiscount) {
+      value -= this.getDiscount('TRADE');
+    }
+    if (this.data.cashDiscount) {
+      value -= this.getDiscount('CASH');
+    }
+    return value * 0.18;
+  }
+  getTotalAmount() {
+    let value = 0;
+    value = this.getProductsAbsoluteValue();
+    if (this.data.tradeDiscount) {
+      value -= this.getDiscount('TRADE');
+    }
+    if (this.data.cashDiscount) {
+      value -= this.getDiscount('CASH');
+    }
+    value -= this.getTaxValue();
+    return value;
+  }
 
   ngOnInit(): void {
     console.log(this.data);
