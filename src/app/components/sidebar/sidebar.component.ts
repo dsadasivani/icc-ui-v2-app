061bb90e584c3fb20107d65997d0ac4fff014bed
Dilip Kumar Patnaik Sadasivani <dsadasivani@gmail.com>
2023-07-27 15:09:22 +0530

    access control added

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +8,14 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   @Output() navBarToggleFlag = new EventEmitter<{ flag: boolean }>();
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   toggleNavBar(): void {
     this.navBarToggleFlag.emit({ flag: true });
+  }
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
